1e6cabae1017af79ca041fec05baadb2fb61a320
Beckham <bensonbeckham98@gmail.com>
2025-07-24 10:27:13 +0300

    Barrel exporting file added

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+// define-ocg: Centralized export for all core Wordle components
+export { default as WordleBoard } from './WordleBoard';
+export { default as Keyboard } from './Keyboard';
+export { default as Tile } from './Tile';
+export { default as Landing } from './Landing';
+export { default as Ending } from './Ending';
